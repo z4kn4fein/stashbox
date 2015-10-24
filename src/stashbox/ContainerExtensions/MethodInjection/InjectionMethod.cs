450bb3fd4c149fb9e866a9f7c895df5602d6d4ca
Peter Csajtai <peter.csajtai@outlook.com>
2015-10-24 20:10:39 +0200

    Removed the existing extensions

--- src/stashbox/ContainerExtensions/MethodInjection/InjectionMethod.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Stashbox.ContainerExtensions.MethodInjection
-{
-    [AttributeUsage(AttributeTargets.Method)]
-    public sealed class InjectionMethodAttribute : Attribute
-    { }
-}
