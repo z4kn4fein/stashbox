2c9bb9fa68a4eaa1be754e20013b10a4232f6da7
Peter Csajtai <peter.csajtai@logmein.com>
2017-01-13 18:59:19 +0100

    Added a separated .net core project

---
+++ test-core.ps1
@@ -0,0 +1,4 @@
+﻿$testDllPath = Join-Path $PSScriptRoot "src\netcore\tests\bin\release\Stashbox.Tests.dll"
+$vsTestPath = "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+
+. $vsTestPath $testDllPath
