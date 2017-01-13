d7c05d925ac9583744054b75c6d0fa8cf2118698
Peter Csajtai <peter.csajtai@outlook.com>
2017-01-14 00:48:17 +0100

    Fixed appveyor-core.yml

--- test-core.ps1
+++
@@ -1,4 +0,0 @@
-﻿$testDllPath = Join-Path $PSScriptRoot "src\netcore\tests\bin\release\Stashbox.Tests.dll"
-$vsTestPath = "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
-
-. $vsTestPath $testDllPath
