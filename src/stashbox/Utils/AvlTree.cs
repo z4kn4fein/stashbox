74d6213933019d7f1121050ec83e1e9522207e19
Peter Csajtai <peter.csajtai@logmein.com>
2017-01-24 20:31:03 +0100

    Few modifications

--- src/stashbox/Utils/AvlTree.cs
+++ src/stashbox/Utils/AvlTree.cs
@@ -1,275 +1,102 @@
 ﻿namespace Stashbox.Utils
 {
-    internal class TreeNode<TValue>
-    {
-        public int Key;
-        public TValue Value;
-        public TreeNode<TValue> Parent;
-        public TreeNode<TValue> Left;
-        public TreeNode<TValue> Right;
-        public sbyte Balance;
-
-        public TreeNode(TValue value, int key, TreeNode<TValue> parent = null)
-        {
-            this.Value = value;
-            this.Parent = parent;
-            this.Key = key;
-        }
-    }
-
     public class AvlTree<TKey, TValue>
     {
-        private TreeNode<TValue> root;
+        private readonly int storedHash;
+        private readonly TValue storedValue;
 
-        public bool Search(TKey key, out TValue value)
+        private readonly AvlTree<TKey, TValue> leftNode;
+        private readonly AvlTree<TKey, TValue> rightNode;
+
+        private readonly int height;
+        private readonly bool isEmpty;
+
+        private AvlTree(int hash, TValue value, AvlTree<TKey, TValue> left, AvlTree<TKey, TValue> right)
         {
-            TreeNode<TValue> node = root;
-            var k = key.GetHashCode();
-
-            while (node != null)
-            {
-                if (k < node.Key)
-                    node = node.Left;
-                else if (k > node.Key)
-                    node = node.Right;
-                else
-                {
-                    value = node.Value;
-                    return true;
-                }
-            }
-
-            value = default(TValue);
-            return false;
+            this.storedHash = hash;
+            this.leftNode = left;
+            this.rightNode = right;
+            this.storedValue = value;
+            this.isEmpty = false;
+            this.height = 1 + (left.height > right.height ? left.height : right.height);
         }
 
-        public void Add(TKey key, TValue value)
+        private AvlTree(int hash, TValue value)
+            : this(hash, value, new AvlTree<TKey, TValue>(), new AvlTree<TKey, TValue>())
+        {
+        }
+
+        public AvlTree()
+        {
+            this.isEmpty = true;
+        }
+
+        public AvlTree<TKey, TValue> Add(TKey key, TValue value)
+        {
+            var hash = key.GetHashCode();
+            return this.Add(hash, value);
+        }
+
+        private AvlTree<TKey, TValue> Add(int hash, TValue value)
+        {
+            if (this.isEmpty)
+                return new AvlTree<TKey, TValue>(hash, value);
+
+            if (hash == this.storedHash)
+                return new AvlTree<TKey, TValue>(hash, value, this.leftNode, this.rightNode);
+
+            var result = hash < this.storedHash
+                ? this.SelfCopy(this.leftNode.Add(hash, value), this.rightNode)
+                : this.SelfCopy(this.leftNode, this.rightNode.Add(hash, value));
+
+            return result.Balance();
+        }
+
+        public TValue Get(TKey key)
         {
             var hash = key.GetHashCode();
 
-            if (root == null)
-            {
-                root = new TreeNode<TValue>(value, hash);
-                return;
-            }
-
-            var node = root;
-
-            while (node != null)
-            {
-                if (hash < node.Key)
-                    if (node.Left == null)
-                    {
-                        node.Left = new TreeNode<TValue>(value, hash, node);
-                        node.Balance--;
-
-                        this.BalanceNode(node);
-                        return;
-                    }
-                    else
-                        node = node.Left;
-                else if (hash > node.Key)
-                    if (node.Right == null)
-                    {
-                        node.Right = new TreeNode<TValue>(value, hash, node);
-                        node.Balance++;
-
-                        this.BalanceNode(node);
-                        return;
-                    }
-                    else
-                        node = node.Right;
-                else
-                    return; //key found
-            }
+            var root = this;
+            while (!root.isEmpty && root.storedHash != hash)
+                root = hash < root.storedHash ? root.leftNode : root.rightNode;
+            return !root.isEmpty ? root.storedValue : default(TValue);
         }
 
-        private void BalanceNode(TreeNode<TValue> node)
+        private AvlTree<TKey, TValue> Balance()
         {
-            while ((node.Balance != 0) && (node.Parent != null))
-            {
-                if (node.Parent.Left == node)
-                    node.Parent.Balance--;
-                else
-                    node.Parent.Balance++;
+            var balance = this.GetBalance();
+            if (balance >= 2)
+                return this.leftNode.GetBalance() == -1 ? this.RotateLeftRight() : this.RotateRight();
 
-                node = node.Parent;
+            if (balance <= -2)
+                return this.rightNode.GetBalance() == 1 ? this.RotateRightLeft() : this.RotateLeft();
 
-                if (node.Balance == -2)
-                {
-                    if (node.Left.Balance == -1)
-                        this.RotateLeft(node);
-                    else
-                        this.RotateRightLeft(node);
-
-                    break;
-                }
-                else if (node.Balance == 2)
-                {
-                    if (node.Right.Balance == 1)
-                        this.RotateRight(node);
-                    else
-                        this.RotateLeftRight(node);
-
-                    break;
-                }
-            }
+            return this;
         }
 
-        private void RotateLeft(TreeNode<TValue> node)
+        private AvlTree<TKey, TValue> RotateLeft()
         {
-            var left = node.Left;
-            left.Parent = node.Parent;
-
-            if (node.Parent == null)
-                this.root = left;
-            else
-            {
-                if (node.Parent.Left == node)
-                    node.Parent.Left = left;
-                else
-                    node.Parent.Right = left;
-            }
-
-            node.Left = left.Right;
-
-            if (node.Left != null)
-                node.Left.Parent = node;
-
-            left.Right = node;
-            node.Parent = left;
-
-            left.Balance = 0;
-            node.Balance = 0;
+            return this.rightNode.SelfCopy(this.SelfCopy(this.leftNode, this.rightNode.leftNode), this.rightNode.rightNode);
         }
 
-        private void RotateRight(TreeNode<TValue> node)
+        private AvlTree<TKey, TValue> RotateRight()
         {
-            var right = node.Right;
-            right.Parent = node.Parent;
-
-            if (node.Parent == null)
-                this.root = right;
-            else
-            {
-                if (node.Parent.Left == node)
-                    node.Parent.Left = right;
-                else
-                    node.Parent.Right = right;
-            }
-
-            node.Right = right.Left;
-
-            if (node.Right != null)
-                node.Right.Parent = node;
-
-            right.Left = node;
-            node.Parent = right;
-
-            right.Balance = 0;
-            node.Balance = 0;
+            return this.leftNode.SelfCopy(this.leftNode.leftNode, this.SelfCopy(this.leftNode.rightNode, this.rightNode));
         }
 
-        private void RotateRightLeft(TreeNode<TValue> node)
+        private AvlTree<TKey, TValue> RotateRightLeft()
         {
-            var left = node.Left;
-            var right = left.Right;
-            right.Parent = node.Parent;
-
-            if (node.Parent == null)
-                this.root = right;
-            else
-            {
-                if (node.Parent.Left == node)
-                    node.Parent.Left = right;
-                else
-                    node.Parent.Right = right;
-            }
-
-            left.Right = right.Left;
-
-            if (left.Right != null)
-                left.Right.Parent = left;
-
-            node.Left = right.Right;
-
-            if (node.Left != null)
-                node.Left.Parent = node;
-
-            right.Left = left;
-            right.Right = node;
-
-            left.Parent = right;
-            node.Parent = right;
-
-            if (right.Balance == -1)
-            {
-                left.Balance = 0;
-                node.Balance = 1;
-            }
-            else if (right.Balance == 0)
-            {
-                left.Balance = 0;
-                node.Balance = 0;
-            }
-            else
-            {
-                left.Balance = -1;
-                node.Balance = 0;
-            }
-
-            right.Balance = 0;
+            return this.SelfCopy(this.leftNode, this.rightNode.RotateRight()).RotateLeft();
         }
 
-        private void RotateLeftRight(TreeNode<TValue> node)
+        private AvlTree<TKey, TValue> RotateLeftRight()
         {
-            var right = node.Right;
-            var left = right.Left;
-            left.Parent = node.Parent;
-
-            if (node.Parent == null)
-                this.root = left;
-            else
-            {
-                if (node.Parent.Left == node)
-                    node.Parent.Left = left;
-                else
-                    node.Parent.Right = left;
-            }
-
-            right.Left = left.Right;
-
-            if (right.Left != null)
-                right.Left.Parent = right;
-
-            node.Right = left.Left;
-
-            if (node.Right != null)
-                node.Right.Parent = node;
-
-            left.Right = right;
-            left.Left = node;
-
-            right.Parent = left;
-            node.Parent = left;
-
-            if (left.Balance == 1)
-            {
-                right.Balance = 0;
-                node.Balance = -1;
-            }
-            else if (left.Balance == 0)
-            {
-                right.Balance = 0;
-                node.Balance = 0;
-            }
-            else
-            {
-                right.Balance = 1;
-                node.Balance = 0;
-            }
-
-            left.Balance = 0;
+            return this.SelfCopy(this.leftNode.RotateLeft(), this.rightNode).RotateRight();
         }
+
+        private AvlTree<TKey, TValue> SelfCopy(AvlTree<TKey, TValue> left, AvlTree<TKey, TValue> right) =>
+            new AvlTree<TKey, TValue>(this.storedHash, this.storedValue, left, right);
+
+        private int GetBalance() => this.leftNode.height - this.rightNode.height;
     }
 }
