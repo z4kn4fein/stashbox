6db641cf3ca235b9b951b4ba79441b63c799ce96
Peter Csajtai <peter.csajtai@logmein.com>
2017-02-13 15:55:38 +0100

    Updated coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -1,16 +1,17 @@
-﻿nuget install coveralls.io -Version 1.3.4 -OutputDirectory .\tools
+﻿nuget install coveralls.net -Version 0.412.0 -OutputDirectory .\tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory .\tools
 nuget install ReportGenerator -Version 2.5.2 -OutputDirectory .\tools
 
 $openCoverPath = Join-Path $PSScriptRoot "tools\OpenCover.4.6.519\tools\OpenCover.Console.exe"
-$coverallsPath = Join-Path $PSScriptRoot "tools\coveralls.io.1.3.4\tools\coveralls.net.exe"
+$coverallsPath = Join-Path $PSScriptRoot "tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe"
 $reportGeneratorPath = Join-Path $PSScriptRoot "tools\ReportGenerator.2.5.2\tools\ReportGenerator.exe"
 $testDllPath = Join-Path $PSScriptRoot "src\stashbox.tests\bin\release\Stashbox.Tests.dll"
 $coverageReportDir = Join-Path $PSScriptRoot "coverageresults"
 
-dir
-
 $arguments = "-oldStyle", "-returntargetcode", "-register:user", "`"-filter:+[*]* -[Stashbox.Tests]* -[Stashbox]*.Utils*`"", "-target:vstest.console.exe", "`"-targetargs:$testDllPath`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
 . $openCoverPath $arguments
+
+dir
+
 . $coverallsPath --serviceName appveyor --opencover -i .\coverage.xml
 . $reportGeneratorPath -verbosity:Info -reports:coverage.xml -targetdir:$coverageReportDir "-assemblyfilters:-Stashbox.Tests*"
