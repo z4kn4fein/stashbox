0ab46098137feb61e458f15b3c58e13378799121
Peter Csajtai <peter.csajtai@outlook.com>
2017-02-14 01:35:09 +0100

    Fixing debug type

--- set-debug-type.ps1
+++ set-debug-type.ps1
@@ -4,5 +4,4 @@ $version = $ENV:APPVEYOR_BUILD_VERSION
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
 $project.Project.PropertyGroup[0].DebugType = "full"
-$project.Project.PropertyGroup[0].DebugSymbols = "true"
 $project.Save($csprojPath)
