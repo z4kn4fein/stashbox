fc8653867ee4c01bcf10aa664e9c70fa58f7f66b
Peter Csajtai <peter.csajtai@outlook.com>
2017-02-14 01:17:44 +0100

    Modified coverage and helper scripts

--- set-debug-type.ps1
+++ set-debug-type.ps1
@@ -3,5 +3,6 @@ param($projectPath)
 $version = $ENV:APPVEYOR_BUILD_VERSION
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
-$project.Project.PropertyGroup[0].Version = $version
+$project.Project.PropertyGroup[0].DebugType = "full"
+$project.Project.PropertyGroup[0].DebugSymbols = "true"
 $project.Save($csprojPath)
