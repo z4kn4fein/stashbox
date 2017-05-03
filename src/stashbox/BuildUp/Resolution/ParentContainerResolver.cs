a1ef389d700cd867442981c4865f485705fc3c17
Peter Csajtai <peter.csajtai@logmein.com>
2017-05-03 13:37:24 +0200

    Small fix

--- src/stashbox/BuildUp/Resolution/ParentContainerResolver.cs
+++ src/stashbox/BuildUp/Resolution/ParentContainerResolver.cs
@@ -7,8 +7,6 @@ namespace Stashbox.BuildUp.Resolution
 {
     internal class ParentContainerResolver : Resolver
     {
-        public override bool SupportsMany => true;
-
         public override bool CanUseForResolution(IContainerContext containerContext, TypeInformation typeInfo) =>
             containerContext.Container.ParentContainer != null && containerContext.Container.ParentContainer.CanResolve(typeInfo.Type, typeInfo.DependencyName);
 
