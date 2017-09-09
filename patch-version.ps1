1a5aa42622680f5313973e3198828e11ec8c7d2a
Peter Csajtai <peter.csajtai@outlook.com>
2017-09-09 03:20:42 +0200

    Merge dev to master (#29)
    * Added sonarqube analysis
    
    * Fixed sonarqube config
    
    * Added version to sonarqube report
    
    * Replaced the sonarqube token with a secured one
    
    * Updated appveyor config
    
    * Fixed appveyor config
    
    * Fixed project file
    
    * Try to fix version patching
    
    * Try to fix version patching
    
    * Falling back to the version patch script due to bug: https://github.com/appveyor/ci/issues/1710
    
    * Added sonarqube badge
    
    * Update README.md [skip ci]
    
    * Update README.md [skip ci]

--- patch-version.ps1
+++ patch-version.ps1
@@ -1,8 +1,9 @@
-param($projectPath, $version)
+param($projectPath, $version, $packageVersion)
 
 Write-Host "Patching with version $version"
 
 $csprojPath = Join-Path $PSScriptRoot $projectPath
 [xml]$project = Get-Content -Path $csprojPath
 $project.Project.PropertyGroup[0].Version = $version
+$project.Project.PropertyGroup[0].PackageVersion = $packageVersion
 $project.Save($csprojPath)
