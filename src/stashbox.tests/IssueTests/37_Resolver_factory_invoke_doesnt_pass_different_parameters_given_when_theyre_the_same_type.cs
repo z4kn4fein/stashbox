2fb2460f240e64c98d10c88f65bac5de7cc3ca1a
Peter Csajtai <peter.csajtai@outlook.com>
2018-01-17 02:33:58 +0100

    More unit tests for #37

--- src/stashbox.tests/IssueTests/37_Resolver_factory_invoke_doesnt_pass_different_parameters_given_when_theyre_the_same_type.cs
+++ src/stashbox.tests/IssueTests/37_Resolver_factory_invoke_doesnt_pass_different_parameters_given_when_theyre_the_same_type.cs
@@ -11,6 +11,16 @@ namespace Stashbox.Tests.IssueTests
         {
             var factory = new StashboxContainer()
                 .RegisterType<IFoo, Foobar>()
+                .ResolveFactory(typeof(IFoo), parameterTypes: new[] { typeof(string), typeof(string) });
+
+            Assert.AreEqual("foobar", ((IFoo)factory.DynamicInvoke("foo", "bar")).Result);
+        }
+
+        [TestMethod]
+        public void Resolver_factory_invoke_doesnt_pass_different_parameters_given_when_theyre_the_same_type_func()
+        {
+            var factory = new StashboxContainer()
+                .RegisterType<IFoo, Foobar>()
                 .Resolve<Func<string, string, IFoo>>();
 
             Assert.AreEqual("foobar", factory("foo", "bar").Result);
