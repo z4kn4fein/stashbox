5e32af3a18ff088775eff793eb802eca43ab7110
Peter Csajtai <peter.csajtai@outlook.com>
2018-01-17 01:14:21 +0100

    Modified the coverage script

--- coverage.ps1
+++ coverage.ps1
@@ -2,18 +2,12 @@
 	exit 0
 } 
 
-nuget install OpenCover -Version 4.6.519 -OutputDirectory .\tools
-nuget install ReportGenerator -Version 2.5.2 -OutputDirectory .\tools
+choco install opencover.portable
+choco install codecov
 
-$openCoverPath = Join-Path $PSScriptRoot "tools\OpenCover.4.6.519\tools\OpenCover.Console.exe"
-$reportGeneratorPath = Join-Path $PSScriptRoot "tools\ReportGenerator.2.5.2\tools\ReportGenerator.exe"
 $testPath = Join-Path $PSScriptRoot "src\stashbox.tests\stashbox.tests.csproj"
 $coverageReportDir = Join-Path $PSScriptRoot "coverageresults"
 
-$env:Path += ";c:\Python34;C:\Python34\Scripts"
-
 $arguments = "-returntargetcode", "-register:user", "`"-filter:+[*]Stashbox.* -[Stashbox.Tests]* -[Stashbox]*.Utils* -[Stashbox]*.Expressions.Compile*`"", "-target:dotnet.exe", "`"-targetargs:test $testPath -f net45 -c Release`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
-. $openCoverPath $arguments
-. pip install codecov
+. OpenCover.Console.exe $arguments
 . codecov -f coverage.xml -X gcov
-. $reportGeneratorPath -verbosity:Info -reports:coverage.xml -targetdir:$coverageReportDir "-assemblyfilters:-Stashbox.Tests*"
