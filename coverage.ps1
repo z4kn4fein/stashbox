a68a0ab32c0748e6045e2ae23fc7db541f672149
Peter Csajtai <peter.csajtai@logmein.com>
2018-09-24 16:12:01 +0200

    Folder reorganization

--- coverage.ps1
+++ coverage.ps1
@@ -5,7 +5,7 @@
 choco install opencover.portable
 choco install codecov
 
-$testPath = Join-Path $PSScriptRoot "src\stashbox.tests\stashbox.tests.csproj"
+$testPath = Join-Path $PSScriptRoot "test\stashbox.tests.csproj"
 $coverageReportDir = Join-Path $PSScriptRoot "coverageresults"
 
 $arguments = "-returntargetcode", "-register:user", "`"-filter:+[*]Stashbox.* -[Stashbox.Tests]* -[Stashbox]*.Utils* -[Stashbox]*.Expressions.Compile*`"", "-target:dotnet.exe", "`"-targetargs:test $testPath -f net45 -c Release`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
