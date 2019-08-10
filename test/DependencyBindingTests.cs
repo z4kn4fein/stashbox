63eb9e60f3f509fd3d62d878a540cd5273c670eb
Peter Csajtai <peter.csajtai@outlook.com>
2019-08-10 02:05:24 +0200

    Add missing file

---
+++ test/DependencyBindingTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stashbox.Tests
+{
+    [TestClass]
+    public class DependencyBindingTests
+    {
+        [TestMethod]
+        public void DependencyBindingTests_Bind_To_The_Same_Type()
+        {
+            var inst = new StashboxContainer()
+                .Register<ITest1, Test1>("test1")
+                .Register<ITest1, Test11>("test2")
+                .Register<ITest1, Test12>("test3")
+                .Register<Test>(ctx => ctx.WithDependencyBinding(typeof(ITest1), "test2"))
+                .Resolve<Test>();
+
+            Assert.IsInstanceOfType(inst.Test1, typeof(Test11));
+            Assert.IsInstanceOfType(inst.Test2, typeof(Test11));
+            Assert.IsInstanceOfType(inst.Test3, typeof(Test11));
+
+        }
+
+        [TestMethod]
+        public void DependencyBindingTests_Bind_To_Different_Types()
+        {
+            var inst = new StashboxContainer()
+                .Register<ITest1, Test1>("test1")
+                .Register<ITest1, Test11>("test2")
+                .Register<ITest1, Test12>("test3")
+                .Register<Test>(ctx => ctx
+                    .WithDependencyBinding("test1", "test1")
+                    .WithDependencyBinding("test2", "test2")
+                    .WithDependencyBinding("test3", "test3"))
+                .Resolve<Test>();
+
+            Assert.IsInstanceOfType(inst.Test1, typeof(Test1));
+            Assert.IsInstanceOfType(inst.Test2, typeof(Test11));
+            Assert.IsInstanceOfType(inst.Test3, typeof(Test12));
+
+        }
+
+        [TestMethod]
+        public void DependencyBindingTests_Override_Typed_Bindings()
+        {
+            var inst = new StashboxContainer()
+                .Register<ITest1, Test1>("test1")
+                .Register<ITest1, Test11>("test2")
+                .Register<ITest1, Test12>("test3")
+                .Register<Test>(ctx => ctx
+                    .WithDependencyBinding("test3", "test3")
+                    .WithDependencyBinding<ITest1>("test2"))
+                .Resolve<Test>();
+
+            Assert.IsInstanceOfType(inst.Test1, typeof(Test11));
+            Assert.IsInstanceOfType(inst.Test2, typeof(Test11));
+            Assert.IsInstanceOfType(inst.Test3, typeof(Test12));
+
+        }
+
+        interface ITest1 { }
+        
+        class Test1 : ITest1
+        { }
+
+        class Test11 : ITest1
+        { }
+
+        class Test12 : ITest1
+        { }
+
+        class Test
+        {
+            public ITest1 Test1 { get; }
+            public ITest1 Test2 { get; }
+            public ITest1 Test3 { get; }
+
+            public Test(ITest1 test1, ITest1 test2, ITest1 test3)
+            {
+                this.Test1 = test1;
+                this.Test2 = test2;
+                this.Test3 = test3;
+            }
+        }
+    }
+}
