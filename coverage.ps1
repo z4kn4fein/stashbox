274123269165336c8deeb432b0d1b0121205befc
Peter Csajtai <peter.csajtai@outlook.com>
2019-08-11 12:58:42 +0200

    Try to fix coverage report generation

--- coverage.ps1
+++ coverage.ps1
@@ -7,7 +7,6 @@ choco install codecov --no-progress
 
 $testPath = Join-Path $PSScriptRoot "test\stashbox.tests.csproj"
 
-$arguments = "-returntargetcode", "-register:user", "`"-filter:+[*]Stashbox.* -[Stashbox.Tests]* -[Stashbox]*.Utils* -[Stashbox]*.Expressions.Compile*`"", "-target:dotnet.exe", "`"-targetargs:test $testPath -f net45 -c Release`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
+$arguments = "-returntargetcode", "-register:user", "`"-filter:+[*]Stashbox.* -[Stashbox.Tests]* -[Stashbox]*.Utils* -[Stashbox]*.Expressions.Compile*`"", "-target:dotnet.exe", "`"-targetargs:test $testPath -f netcoreapp2.0 -c Release`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
 . OpenCover.Console.exe $arguments
-. type coverage.xml
 . codecov -f coverage.xml 
