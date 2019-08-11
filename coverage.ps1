9628f189d6a9aae0ea4b82c57ddf6b12cbbbf422
Peter Csajtai <peter.csajtai@outlook.com>
2019-08-11 02:16:58 +0200

    Try to fix coverage report

--- coverage.ps1
+++ coverage.ps1
@@ -3,11 +3,11 @@
 } 
 
 choco install opencover.portable --no-progress
-choco install codecov --version 1.6.1 --no-progress
+choco install codecov --no-progress
 
 $testPath = Join-Path $PSScriptRoot "test\stashbox.tests.csproj"
-$coverageReportDir = Join-Path $PSScriptRoot "coverageresults"
 
 $arguments = "-returntargetcode", "-register:user", "`"-filter:+[*]Stashbox.* -[Stashbox.Tests]* -[Stashbox]*.Utils* -[Stashbox]*.Expressions.Compile*`"", "-target:dotnet.exe", "`"-targetargs:test $testPath -f net45 -c Release`"", "-output:coverage.xml", "-skipautoprops", "-hideskipped:All"
+. type coverage.xml
 . OpenCover.Console.exe $arguments
 . codecov -f coverage.xml
