674fb53c80acd29d261615b7ed995b960289b209
Peter Csajtai <peter.csajtai@outlook.com>
2019-11-25 16:23:11 +0100

    More tests for #72

--- test/IssueTests/72_Default_lifetime_set.cs
+++ test/IssueTests/72_Default_lifetime_set.cs
@@ -10,7 +10,7 @@ namespace Stashbox.Tests.IssueTests
         public void Ensure_Default_Lifetime_Used_When_Custom_Not_Set()
         {
             var lifeTime = new ScopedLifetime();
-            var mappings = new StashboxContainer(c => c.WithDefaultLifetime(new ScopedLifetime()))
+            var mappings = new StashboxContainer(c => c.WithDefaultLifetime(lifeTime))
                 .Register<Test>().GetRegistrationMappings();
 
             var reg = mappings.First();
@@ -18,6 +18,18 @@ namespace Stashbox.Tests.IssueTests
             Assert.Equal(typeof(Test), reg.Key);
             Assert.IsType<ScopedLifetime>(reg.Value.RegistrationContext.Lifetime);
             Assert.NotSame(lifeTime, reg.Value.RegistrationContext.Lifetime);
+        }
+
+        [Fact]
+        public void Ensure_Custom_Lifetime_Used_When_Both_Set()
+        {
+            var mappings = new StashboxContainer(c => c.WithDefaultLifetime(new ScopedLifetime()))
+                .Register<Test>(c => c.WithSingletonLifetime()).GetRegistrationMappings();
+
+            var reg = mappings.First();
+
+            Assert.Equal(typeof(Test), reg.Key);
+            Assert.IsType<SingletonLifetime>(reg.Value.RegistrationContext.Lifetime);
         }
 
         class Test { }
