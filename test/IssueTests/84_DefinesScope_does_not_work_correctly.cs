aa3aa91309af780ac7ec6e7e93dda41b7803268e
Peter Csajtai <peter.csajtai@outlook.com>
2020-06-05 12:18:43 +0200

    Test for #84

---
+++ test/IssueTests/84_DefinesScope_does_not_work_correctly.cs
@@ -0,0 +1,50 @@
+﻿using Stashbox.Lifetime;
+using Xunit;
+
+namespace Stashbox.Tests.IssueTests
+{
+    public class DefinesScopeSoesNotWorkCorrectly
+    {
+        [Fact]
+        public void DefinesScope_does_not_work_correctly()
+        {
+            using var container = new StashboxContainer()
+                .Register<ProductCache>(c => c
+                    .DefinesScope()
+                    .WithLifetime(Lifetimes.Singleton)
+                    .WithoutDisposalTracking())
+                .Register<ProductService>()
+                .RegisterScoped<ProductRepository>();
+
+            var inst = container.Resolve<ProductService>();
+
+            Assert.NotSame(inst.Repo, inst.Cache.Repo);
+        }
+    }
+
+    class ProductCache
+    {
+        public ProductRepository Repo { get; }
+
+        public ProductCache(ProductRepository repo)
+        {
+            Repo = repo;
+        }
+
+    }
+
+    class ProductService
+    {
+        public ProductRepository Repo { get; }
+        public ProductCache Cache { get; }
+
+        public ProductService(ProductRepository repo, ProductCache cache)
+        {
+            Repo = repo;
+            Cache = cache;
+        }
+    }
+
+    class ProductRepository
+    { }
+}
