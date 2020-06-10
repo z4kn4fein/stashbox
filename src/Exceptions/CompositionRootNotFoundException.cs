cb102f3435f94ce84d740cd2ea38abe6cce3c650
Peter Csajtai <peter.csajtai@outlook.com>
2020-06-10 14:15:16 +0200

    Remove unused property

--- src/Exceptions/CompositionRootNotFoundException.cs
+++ src/Exceptions/CompositionRootNotFoundException.cs
@@ -15,20 +15,13 @@ namespace Stashbox.Exceptions
     public class CompositionRootNotFoundException : Exception
     {
         /// <summary>
-        /// The scanned assembly.
-        /// </summary>
-        public Assembly Assembly { get; }
-
-        /// <summary>
         /// Constructs a <see cref="CompositionRootNotFoundException"/>.
         /// </summary>
         /// <param name="assembly">The scanned assembly.</param>
         /// <param name="innerException">The inner exception.</param>
         public CompositionRootNotFoundException(Assembly assembly, Exception innerException = null)
             : base($"No ICompositionRoot found in the given assembly: {assembly.FullName}.", innerException)
-        {
-            this.Assembly = assembly;
-        }
+        { }
 
 #if HAS_SERIALIZABLE
         /// <inheritdoc />
