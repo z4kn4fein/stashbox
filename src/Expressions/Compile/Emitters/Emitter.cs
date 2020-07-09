1577cb228b1b34b9765fad63c6b4264b1d8ce695
Peter Csajtai <peter.csajtai@outlook.com>
2020-07-09 12:28:30 +0200

    Remove unnecessary using

--- src/Expressions/Compile/Emitters/Emitter.cs
+++ src/Expressions/Compile/Emitters/Emitter.cs
@@ -3,7 +3,6 @@ using Stashbox.Expressions.Compile.Extensions;
 using Stashbox.Utils.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection.Emit;
 
