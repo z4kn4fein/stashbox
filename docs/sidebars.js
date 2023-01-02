7f7b1a6c3114794f0d6a2ffcdb9b0c430488a2c6
Peter Csajtai <peter.csajtai@outlook.com>
2023-01-02 20:52:26 +0100

    Docs style

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -4,6 +4,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Get started',
+      collapsed: false,
       items: [
         'getting-started/overview',
         'getting-started/introduction',
@@ -13,6 +14,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Guides',
+      collapsed: false,
       items: [
         'guides/basics',
         'guides/advanced-registration',
@@ -24,6 +26,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Configuration',
+      collapsed: false,
       items: [
         'configuration/registration-configuration',
         'configuration/container-configuration',
@@ -32,6 +35,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Advanced',
+      collapsed: false,
       items: [
         'advanced/generics',
         'advanced/decorators',
@@ -43,6 +47,7 @@ const sidebars = {
     {
       type: 'category',
       label: 'Diagnostics',
+      collapsed: false,
       items: [
         'diagnostics/validation',
         'diagnostics/utilities',
