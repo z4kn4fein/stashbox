f01c416e4aa301002d744e633306b399dd6a53e6
Peter Csajtai <peter.csajtai@outlook.com>
2026-04-10 23:10:01 +0200

    Update tests

--- test/IssueTests/228_Stashbox_does_not_handle_Optional_correctly.cs
+++ test/IssueTests/228_Stashbox_does_not_handle_Optional_correctly.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 
-using System;
 using System.Runtime.InteropServices;
 using Xunit;
 
@@ -13,11 +12,19 @@ public class StashboxDoesNotHandleOptionalCorrectly
     {
         using var container = new StashboxContainer();
         container.Register<B>();
+        container.Register<C>();
+        container.Register<D>();
 
         Assert.NotNull(container.Resolve<B>());
+        Assert.NotNull(container.Resolve<C>());
+        Assert.NotNull(container.Resolve<D>());
     }
     
     private class A { }
     
     private class B([Optional] A? a) { }
+    
+    private class C([Optional] int? a) { }
+    
+    private class D([Optional] string? a) { }
 }
