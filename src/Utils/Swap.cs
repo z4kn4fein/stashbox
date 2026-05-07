95316a4956978ac79df02dc5beea3aeb90831e0f
Peter Csajtai <peter.csajtai@outlook.com>
2026-05-07 14:03:38 +0200

    Update Swap.cs

--- src/Utils/Swap.cs
+++ src/Utils/Swap.cs
@@ -10,7 +10,7 @@ internal static class Swap
     public static bool SwapValue<T1, T2, T3, T4, TValue>(ref TValue refValue, Func<T1, T2, T3, T4, TValue, TValue> valueFactory, T1 t1, T2 t2, T3 t3, T4 t4)
         where TValue : class
     {
-        var currentValue = refValue;
+        var currentValue = Volatile.Read(ref refValue);
         var newValue = valueFactory(t1, t2, t3, t4, currentValue);
 
         var original = Interlocked.CompareExchange(ref refValue, newValue, currentValue);
@@ -30,7 +30,7 @@ internal static class Swap
 
         while (true)
         {
-            var currentValue = refValue;
+            var currentValue = Volatile.Read(ref refValue);
             var newValue = valueFactory(t1, t2, t3, t4, currentValue);
 
             var original = Interlocked.CompareExchange(ref refValue, newValue, currentValue);
